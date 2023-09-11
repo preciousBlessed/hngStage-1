4ac8107b188646c32f4a8ba6d1e52b4d80ae8887
preciousBlessed <tobechukwublessed30@yahoo.com>
2023-09-11 12:30:47 +0100

    Alpha commit v4 url fix

--- routes/router.js
+++ routes/router.js
@@ -15,7 +15,8 @@ firstRoute.get("/", (req, res) => {
   res.json({
     slack_name: req.query.slack_name || "",
     current_day: dow[new Date().getDay()],
-    utc_time: new Date(new Date().getTime() + 60 * 60 * 1000),
+    // utc_time: new Date(new Date().getTime() + 60 * 60 * 1000),
+    utc_time: new Date(new Date()),
     track: req.query.track || "",
     github_file_url:
       "https://github.com/preciousBlessed/hngStage-1/blob/master/app.js",
