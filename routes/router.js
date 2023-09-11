ff80db33f1e2240be8172eb247c6927c4e5257cf
preciousBlessed <tobechukwublessed30@yahoo.com>
2023-09-11 12:08:58 +0100

    Alpha commit v2

--- routes/router.js
+++ routes/router.js
@@ -2,15 +2,25 @@ const express = require("express");
 
 const firstRoute = express.Router();
 
+const dow = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
 firstRoute.get("/", (req, res) => {
   res.json({
-    slack_name: req.query.slack_name,
-    current_day: new Date().getDay(),
-    utc_time: new Date() + 60 * 60 * 1000,
-    track: req.query.track,
-    github_file_url: "",
-    github_repo_url: "",
-    status_code: 200,
+    slack_name: req.query.slack_name || "",
+    current_day: dow[new Date().getDay()],
+    utc_time: new Date(new Date().getTime() + 60 * 60 * 1000),
+    track: req.query.track || "",
+    github_file_url:
+      "https://github.com/preciousBlessed/hngStage-1/blob/master/app.js",
+    github_repo_url: "https://github.com/preciousBlessed/hngStage-1",
+    status_code: res.statusCode,
   });
 });
 
---
+++ test.js
@@ -0,0 +1,6 @@
+const newDate = new Date();
+const currentTime = new Date().getTime();
+console.log(new Date(currentTime + 60 * 60 * 1000));
+console.log(
+  "This is just for testing purpose, I used it to figure out my date format"
+);
